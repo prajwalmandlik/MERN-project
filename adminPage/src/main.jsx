f6be69f77688042aa24157dddd1a46cfc658b0e6
Prajwal Mandlik <114695571+prajwalmandlik@users.noreply.github.com>
2023-04-06 23:15:18 +0530

    Update main.jsx

--- adminPage/src/main.jsx
+++ adminPage/src/main.jsx
@@ -7,7 +7,7 @@ import { createContext } from 'react'
 import { useState } from 'react'
 import schema from './componentes/schema'
 
-export const server = "http://localhost:4000/api/v1";
+export const server = "https://adhikar-backend.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
