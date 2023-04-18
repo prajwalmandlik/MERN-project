bb13f25f6cbca75d1df70bd535b54ef717658a1b
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-04-18 23:17:29 +0530

    server link update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,5 +18,5 @@ root.render(
 );
 
 
-// export const server = "https://adhikar-backend.onrender.com/api/v1"
-export const server = "http://localhost:4000/api/v1"
+export const server = "https://adhikar-backend.onrender.com/api/v1"
+// export const server = "http://localhost:4000/api/v1"
